9f8774a2ded430ee7acaa608720fabfb753fa246
Mayank Barakoti <your_github_mankubarakoti@example.com>
2025-11-23 14:26:56 +0530

    1v1 chat update: added enter+esc support + system messages fixed

--- server.js
+++ server.js
@@ -248,7 +248,7 @@ wss.on("connection", (ws) => {
 });
 
 // Start Server 
-const PORT = process.env.PORT || 4004;
+const PORT = process.env.PORT || 4001;
 server.listen(PORT, () => {
   console.log(`✅ Mystery Meet 💬 server running on http://localhost:${PORT}`);
 });
